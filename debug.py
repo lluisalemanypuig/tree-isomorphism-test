a35275fe773c03933f40cced81c223a2c260596c
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2019-10-03 11:06:05 +0200

    Updated LICENSE

--- debug.py
+++ debug.py
@@ -1,3 +1,23 @@
+"""
+tree-isomorphism-test - An algorithm for fast tree isomorphism test
+Copyright (C) 2019  Lluís Alemany Puig
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact: Lluís Alemany Puig (lluis.alemany.puig@gmail.com)
+"""
+
 from sys import argv
 from debug_small import run_small_tests
 from debug_medium import run_medium_tests
@@ -37,6 +57,12 @@ if len(argv) < 2:
 	exit(1)
 
 test_type = None
+
+print("tree-Isomorphism-test  Copyright (C) 2019  Lluís Alemany Puig")
+print("This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.")
+print("This is free software, and you are welcome to redistribute it")
+print("under certain conditions; type `show c' for details.")
+
 
 if len(argv) == 2:
 	if argv[1] == "help":
--- debug_medium.py
+++ debug_medium.py
@@ -1,3 +1,23 @@
+"""
+tree-isomorphism-test - An algorithm for fast tree isomorphism test
+Copyright (C) 2019  Lluís Alemany Puig
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact: Lluís Alemany Puig (lluis.alemany.puig@gmail.com)
+"""
+
 from random import sample, shuffle
 import networkx as nx
 from debug_aux import check_all_pairs
